c3997d3689b1f6be4cecebc5c3c993bf708c965b
Shaharli <shaharli712@gmail.com>
2016-07-05 18:29:53 +0300

    back btn fixed

--- app/src/main/java/com/avigezerit/perportion/MainActivity.java
+++ app/src/main/java/com/avigezerit/perportion/MainActivity.java
@@ -3,7 +3,6 @@ package com.avigezerit.perportion;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
         mtNumOfSlice = (EditText) findViewById(R.id.slice_amount);
@@ -188,4 +185,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent backToMain = new Intent(this, CategoriesActivity.class);
+        startActivity(backToMain);
+    }
 }
